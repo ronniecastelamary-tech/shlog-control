e7160abcb56a2de9b3231ae570aaba6476c9303b
ronniecastelamary-tech <ronnie.castelamary@gmail.com>
2026-07-15 15:40:57 -0300

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = 'https://dxrhzkyptanpkekcrfip.supabase.co'
-const SUPABASE_ANON_KEY = 'COLE_AQUI_SUA_CHAVE_COMPLETA'
+const SUPABASE_URL = 'https://xxxxx.supabase.co'
+const SUPABASE_ANON_KEY = 'sb_publishable_xxxxx'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
